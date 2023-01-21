5186a3c60e11f1cddf19c75229208d1090b511af
kate0427 <114397454+kate0427@users.noreply.github.com>
2023-01-21 11:06:35 +0100

    setSearch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <header>Dictionary</header>
+        <Dictionary />
+        <footer className="text-center">
+          <small>Coded by</small>
+        </footer>
+      </div>
     </div>
   );
 }
